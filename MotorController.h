92b73fd58ba5b8d8f647f8fa58e3d1d87ada4013
Liam Telenko <telenkol@mcmaster.ca>
2023-11-24 00:08:08 -0500

    Add support for power control through PWM. Added voltage limit and proportional PWM output scaling

--- MotorController.h
+++ MotorController.h
@@ -16,6 +16,11 @@
 // =================
 enum direction {FORWARD, BACKWARD, STOPPED};
 
+// ====================
+// Constant definition 
+// ====================
+#define CONTROLLER_INPUT_VOLTAGE 5
+
 
 class MotorController {
   
@@ -25,6 +30,8 @@ class MotorController {
   // - PIN_PWM:           Reference to PWM pin used to control motor
   //                      speed.
   // - current_direction: The current direction of the motor
+  // - voltage_limit:     The maximum allowed output voltage
+  // - power:             The power level output from the controller
   // ==================================================================
   private:
     byte PIN_EN_1;
@@ -32,6 +39,9 @@ class MotorController {
     byte PIN_PWM;
 
     direction current_direction;
+
+    byte  voltage_limit;
+    float power;
 
 
     // =======================================================
@@ -41,11 +51,29 @@ class MotorController {
 
   public:
     // =================================================
-    // Constructor. Takes enable and PWM pins as inputs
+    // Constructor: Takes enable and PWM pins as inputs
     // =================================================
     MotorController(byte PIN_EN_1, byte PIN_EN_2, byte PIN_PWM);
 
+
+    // ========================================================
+    // setDirection: Takes direction enum and sets enable pins
+    // ========================================================
     void setDirection(direction dir);
+
+
+    // ========================================================
+    // setPower: Sets the speed of the motor by controlling
+    //           the PWM output as a percentage.
+    // ========================================================
+    void setPower(float power);
+
+
+    // ========================================================
+    // setVoltageLimit: Configures the max output voltage from
+    //                  the controller.
+    // ========================================================
+    void setVoltageLimit(byte voltage);
 
 };
 
