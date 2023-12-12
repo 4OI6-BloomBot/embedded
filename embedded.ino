1dce950c3c0dc9db333a7ebad5360f2c4e436036
mailtelenko <25699456+mailtelenko@users.noreply.github.com>
2023-12-11 23:29:28 -0500

    Update embedded.ino to support changes to TimedLoop. Updated comments

--- embedded.ino
+++ embedded.ino
@@ -5,24 +5,34 @@
 
 */
 
-// ==========
+// =====================================================
 // Includes
-// ==========
+// =====================================================
 #include "src/MotorController/MotorController.h"
 #include "src/GPS/GPS.h"
 
+
+// =====================================================
+// Defines
+// TODO: Move me to a config header file
+// =====================================================
 #define DEBUG
 
-// =======================
+
+// =====================================================
 // Variable Declaration
-// =======================
+// Note: For any classes that extend TimedLoop the 
+//       NUM_TIMEDLOOP_OBJECTS macro needs to be
+//       updated to properly reflect the number of 
+//       children.
+// =====================================================
 // MotorController test(7, 8, 9);
 GPS gps(6, 7);
 
 
-// ====================================
+// =====================================================
 // setup() - Runs once during power-up
-// ====================================
+// =====================================================
 void setup() {
   #ifdef DEBUG
     Serial.begin(115200);
@@ -30,9 +40,21 @@ void setup() {
 }
 
 
-// ============================================================
-// loop() - Main program loop, runs continuously after setup()
-// =============================================================
+// =====================================================
+// loop() - Main program loop, runs continuously
+//          after setup()
+// =====================================================
 void loop() {
-  gps.getLocation();
+
+  // =====================================================
+  // Check each of the timed objects and run their loops
+  // if it is time.
+  // =====================================================
+  TimedLoop::tryEvents();
+  
+  // =====================================================
+  // Wait for the next scheduled event
+  // =====================================================
+  // TODO: Change to low power mode enable/sleep
+  delay(TimedLoop::getTimeToNextEvent());
 }
