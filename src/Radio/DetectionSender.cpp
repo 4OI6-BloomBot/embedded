42c82b19a000417c2bedfd88da6e622d7c5760ee
mailtelenko <25699456+mailtelenko@users.noreply.github.com>
2024-03-21 14:53:17 -0400

    Add sensors to DetectionSender

--- src/Radio/DetectionSender.cpp
+++ src/Radio/DetectionSender.cpp
@@ -29,6 +29,8 @@ void DetectionSender::loop() {
   if (!this->isConfigured) return;
 
   this->sendTemp();
+  this->sendTurb();
+  this->sendFluoro();
 }
 
 
@@ -44,33 +46,71 @@ void DetectionSender::configure(TURB *tb, TEMP *tp, FLUORO *fl) {
 }
 
 
-// =========================================================================
-// sendLocation - Add a packet with the current location data to the Tx
-//                queue.
-// =========================================================================
-bool DetectionSender::sendSensorData() {
-}
-
-
-// ======================================
-// Get temperature sensor
-// ======================================
+// =============================================================
+// sendTemp - Get data from the temp sensor and queue a packet
+//            TODO: These methods are repeat code. Should be 
+//                  combined into a single method.
+//            TODO: The sensors are repeating methods/code but
+//                  do not have a parent class. Should add one.
+// =============================================================
 void DetectionSender::sendTemp() {
   if (newSensorData(this->temp, this->temp_last_access)) {
     this->temp_last_access = millis();
     
-    float temp_val = temp->getTempOut();
+    float val = temp->getTempOut();
 
     // If the data is valid, create the packet
-    if (temp_val != -1) {
-      Temperature *tempPkt = new Temperature();
-      tempPkt->addVal(temp_val);
-      this->packet_handler->queuePacket(tempPkt);
+    if (val != -1) {
+      Temperature *pkt = new Temperature();
+      pkt->addVal(val);
+      this->packet_handler->queuePacket(pkt);
     }
   }
 }
 
 
+// =============================================================
+// sendTurb - Get data from the temp sensor and queue a packet
+// =============================================================
+void DetectionSender::sendTurb() {
+  if (newSensorData(this->temp, this->turb_last_access)) {
+    this->turb_last_access = millis();
+    
+    float val = turb->getTurbOut();
+
+    // If the data is valid, create the packet
+    if (val != -1) {
+      Turbidity *pkt = new Turbidity();
+      pkt->addVal(val);
+      this->packet_handler->queuePacket(pkt);
+    }
+  }
+}
+
+
+// =============================================================
+// sendTemp - Get data from the temp sensor and queue a packet
+// =============================================================
+void DetectionSender::sendFluoro() {
+  if (newSensorData(this->fluoro, this->fluoro_last_access)) {
+    this->fluoro_last_access = millis();
+    
+    float val = fluoro->getFluoroOut();
+
+    // If the data is valid, create the packet
+    if (val != -1) {
+      Fluorescence *pkt = new Fluorescence();
+      pkt->addVal(val);
+      this->packet_handler->queuePacket(pkt);
+    }
+  }
+}
+
+
+// ==========================================================
+// Return true if the sensor has been updated since we last
+// sent a value.
+// ==========================================================
 bool DetectionSender::newSensorData(TimedLoop* s, unsigned long int last_run) {
   return s->getLastRuntime() > last_run;
 }
